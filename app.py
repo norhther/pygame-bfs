66d4016c1125e42dbc03d803c53e2bf024c9a30f
norhther <omarlopezrubio@gmail.com>
2021-07-26 16:52:47 +0200

    Add one_step method

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from board import Board
 
 
 if __name__ == "__main__":
-    b = Board(5,5)
-    b.bfs()
+    b = Board(10,10,random_start = True)
+    b.solve_with_steps()
--- board.py
+++ board.py
@@ -23,13 +23,13 @@ class BoxType(Enum):
         else:
             return "E"
 
-
+"""
 @dataclass
 class Box():
     x: int
     y: int
     Type: BoxType
-
+"""
 
 def chalk_print(val):
     if val == BoxType.WALL:
@@ -43,13 +43,20 @@ def chalk_print(val):
 
 
 class Board():
-    def __init__(self, w: int, h: int, s_x=0, s_y=0):
+    def __init__(self, w: int, h: int, s_x=0, s_y=0, random_start = False, random_objects = 5):
         self.w: int = w
         self.h: int = h
         self.matrix: list[list[[[int, int], BoxType]]] = \
             [[[[x, y], BoxType.SPACE] for y in range(w)] for x in range(h)]
+
         self.start_x = s_x
         self.start_y = s_y
+        if random_start:
+            self._random_start(random_objects)
+        self.todo    = []
+        self.start   = True
+        self.end     = False
+        self.found   = False
 
     def _create_random_object(self, obj=BoxType.WALL):
         x = random.randint(0, self.h - 1)
@@ -59,13 +66,21 @@ class Board():
         if obj == BoxType.ORIGIN:
             self.start_x = x
             self.start_y = y
+        return x,y
 
-    def _random_start(self, n=15):
+    def _random_start(self, n=5):
+        self.todo    = []
+        self.start   = True
+        self.end     = False
+        self.found   = False
         for i in range(n):
             self._create_random_object()
 
-        self._create_random_object(obj=BoxType.END)
-        self._create_random_object(obj=BoxType.ORIGIN)
+        end_x, end_y       = self._create_random_object(obj=BoxType.END)
+        origin_x, origin_y = self._create_random_object(obj=BoxType.ORIGIN)
+        # Avoid overlappin
+        while origin_x == end_x and end_y == origin_y:
+            origin_x, origin_y = self._create_random_object(obj=BoxType.ORIGIN)
 
     def __str__(self):
         res: str = ""
@@ -81,11 +96,37 @@ class Board():
             (self.matrix[x][y][1] == BoxType.SPACE
                 or self.matrix[x][y][1] == BoxType.END)
 
+
+    def oneStep(self):
+        offsets_x = [1, 0, 0, -1]
+        offsets_y = [0, 1, -1, 0]
+        if self.start:
+            self.start = False
+            self.todo  = [self.matrix[self.start_x][self.start_y]]
+        
+        if not self.todo:
+            self.end = True
+
+        else:
+            current_BoxType = self.todo.pop(0)
+            current_x = current_BoxType[0][0]
+            current_y = current_BoxType[0][1]
+            for o_x, o_y in zip(offsets_x, offsets_y):
+                if self._isGood(o_x + current_x, o_y + current_y) and \
+                    self.matrix[o_x + current_x][o_y + current_y][1] != BoxType.VISITED:
+                    self.todo.append(self.matrix[o_x + current_x][o_y + current_y])
+                    if self.matrix[o_x + current_x][o_y + current_y][1] == BoxType.END:
+                        self.end = self.found = True
+                        return
+                    self.matrix[o_x + current_x][o_y + current_y][1] = BoxType.VISITED
+
+    def solve_with_steps(self, visualize = True):
+        while not self.end and not self.found:
+            if visualize:
+                print(self)
+            self.oneStep()
+
     def bfs(self, rand=True):
-        """
-            BFS implementation.
-            Pre: (init_x,init_y) can NOT be in the same location as BoxType.End
-        """
         if rand:
             self._random_start()
 
